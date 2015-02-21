859769632f637d80102ccc71eea5b7765fda3bb0
Ismail Uddin <ismail.sameeuddin@gmail.com>
2015-02-21 20:58:35 +0000

    Clean up code and syntax errors

--- dna-flashing-led/nu_blink.py
+++ dna-flashing-led/nu_blink.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-from bio-seq import parse_FASTA
+from bioseq import parse_FASTA
 
 dna_file = sys.argv[1]
 database = parse_FASTA(dna_file)
@@ -15,30 +15,30 @@ GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 
 
-pwmRed = GPIO.PWM(18, 500)
-pwmGreen = GPIO.PWM(23, 500)
-pwmBlue = GPIO.PWM(24, 500)
+red = GPIO.PWM(18, 500)
+green = GPIO.PWM(23, 500)
+blue = GPIO.PWM(24, 500)
 
 def blink(nucleotide):
 	if nucleotide == 'A':
-		pwmRed.start(100)
-		pwmGreen.start(0)
-		pwmBlue.start(100)
+		red.start(100)
+		green.start(0)
+		blue.start(100)
 		print('A (purple)')
 	elif nucleotide == 'T':
-		pwmRed.start(0)
-		pwmGreen.start(0)
-		pwmBlue.start(100)
+		red.start(0)
+		green.start(0)
+		blue.start(100)
 		print('T (blue)')
 	elif nucleotide == 'C':
-		pwmRed.start(0)
-		pwmGreen.start(100)
-		pwmBlue.start(0)
+		red.start(0)
+		green.start(100)
+		blue.start(0);
 		print('C (green)')
 	elif nucleotide == 'G':
-		pwmRed.start(0)
-		pwmGreen.start(100)
-		pwmBlue.start(100)
+		red.start(0)
+		green.start(100)
+		blue.start(100)
 		print('G (cyan)')
 
 while True:
