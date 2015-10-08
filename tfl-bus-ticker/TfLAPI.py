5a909457818acf18b39b64719a01350dcab02ffe
Ismail Uddin <ismail.sameeuddin@gmail.com>
2015-10-08 23:12:19 +0100

    TfL Bus Ticker
    Code upload

---
+++ tfl-bus-ticker/TfLAPI.py
@@ -0,0 +1,123 @@
+import csv
+import urllib2
+import json
+
+class TfLBusArrivalsAPI:
+	def __init__(self):
+		self.naptanDict = {}
+		self.busStopDict = {}
+
+		# row[1]     Bus stop code
+		# row[2]     Naptan ID
+		# row[3]     Stop name
+
+		with open('data/tfl-bus-stops.csv') as csvfile:
+		    reader = csv.reader(csvfile,delimiter=',')
+		    for row in reader:
+		        self.naptanDict[row[3]] = str(row[2])
+		        self.busStopDict[row[1]] = str(row[2])
+
+	def searchBusStop(self, queryString):
+		"""
+		Returns a list of matching bus stops to queried bus stop name, 
+		and their respective Naptan IDs
+
+		# Arugments
+		queryString          String value for the bus stop name
+
+		# Usage
+		$  from TfLAPI import *
+		$  tfl = TfLBusArrivalsAPI()
+
+		$  tfl.searchBusStop('mile end')
+		"""
+		outputList = {}
+
+		for busStopName, naptanID in self.naptanDict.items():
+		  if queryString.upper() in str(busStopName):
+		      outputList[str(busStopName)] = str(naptanID)
+		  else:
+		      pass
+		  
+		if not outputList:
+		  print("No matching station found")
+		  
+
+		for busStopName, naptanID in outputList.items():
+		  print("%s, %s" % (busStopName, naptanID))
+
+	def returnTfLJSON(self, **kwargs):
+		"""
+		Returns a JSON style Python list containing all information
+		from TfL regarding bus arrivals at specified bus stop.
+		"""
+
+		try:
+			naptanID = kwargs['naptan_id']
+		except KeyError:
+			try:
+		  		busStopCode = str(kwargs['bus_stop_code'])
+		  		naptanID = self.busStopDict[busStopCode]
+			except KeyError:
+				print("You have not specified a bus stop code (bus_stop_code='1234' or Naptan ID (naptan_id='4905N')")
+
+
+		data = urllib2.urlopen("https://api.tfl.gov.uk/StopPoint/" + "%s" % naptanID + "/arrivals")
+		string_data = data.read()[2:-2]
+		jsonList = string_data.split('},{')
+
+		jsonObject = []
+
+		for element in jsonList:
+			json_format = '{' + element + '}'
+			jsonObject.append(json.loads(json_format))
+
+		return jsonObject
+
+	def queryBusArrival(self, **kwargs):
+		"""
+		Returns a dictionary of bus arrival times in minutes for 
+		the queried bus line at the specified bus stop.
+
+		# Arguments
+		bus_line              String value for the bus line
+
+		bus_stop_code         Integer value for the bus stop code
+		### OR (not both)
+		naptan_id             String value for the Naptan ID 
+		                      for bus stop (obtained using searchBusStop())
+		               
+		# Usage
+		$  from TfLAPI import *
+		$  tfl = TfLBusArrivalsAPI()
+
+		$  tfl.queryBusArrival(bus_line='205', bus_stop_code=48439)
+		"""
+
+		try:
+			naptanID = kwargs['naptan_id']
+		except KeyError:
+			try:
+		  		busStopCode = str(kwargs['bus_stop_code'])
+		  		naptanID = self.busStopDict[busStopCode]
+			except KeyError:
+				print("You have not specified a bus stop code (bus_stop_code='1234' or Naptan ID (naptan_id='49015N')")
+
+		try:
+			busLine = kwargs['bus_line']
+		except KeyError:
+			print("You have not specified a bus line (bus_line='339')")
+
+		jsonObject = self.returnTfLJSON(bus_line=kwargs['bus_line'], naptan_id=str(naptanID))
+		
+		busArrivalTimes = []
+
+		for entry in jsonObject:
+			if busLine in entry.values():
+				arrivalMinutes = int(entry['timeToStation'])/60.0
+				busArrivalTimes.append(arrivalMinutes)
+
+		busArrivalTimes.sort()
+
+		return busArrivalTimes
+		 
---
+++ tfl-bus-ticker/tfl-bus-ticker.py
@@ -0,0 +1,65 @@
+# TfL Bus arrivals ticker
+# Ismail Uddin, 2015
+# www.scienceexposure.com
+
+from TfLAPI import *
+import Adafruit_CharLCD as LCD
+import time
+import sys
+
+bSC = sys.argv[1]
+
+#                 RS,EN,D4,D5,D6,D7,Co,Ro,BL
+RPi_PIN_config = [27,22,25,24,23,18,16, 2, 4]
+
+if  sys.argv[1] == "i2c":
+	lcd = LCD.Adafruit_CharLCDPlate()
+else:
+	lcd = LCD.Adafruit_CharLCD(*RPi_PIN_config)
+	
+lcd.clear()
+
+lcd.message('Initialising...')
+lcd.show_cursor(True)
+lcd.blink(True)
+tfl = TfLBusArrivalsAPI()
+
+
+lcd.clear()
+lcd.message('Fetching TfL bus\narrivals...')
+lcd.blink(True)
+
+def fetchBusArrivals():
+	lcd.show_cursor(False)
+	lcd.blink(False)
+	try:
+		jsonObject = tfl.returnTfLJSON(bus_stop_code=bSC)
+	except urllib2.URLError:
+		lcd.clear()
+		print("Unable to connect to Internet...")
+		lcd.message("Unable to connect \nto Internet...")
+		
+	busLineDestinationTime = []
+
+	for entry in jsonObject:
+		bLDT = []
+		bLDT.append(entry['lineName'])
+		bLDT.append(entry['destinationName'])
+		bLDT.append(int(entry['timeToStation'])/60.0)
+		busLineDestinationTime.append(bLDT)
+
+	arrivalsList = sorted(busLineDestinationTime, key=lambda x:x[2])
+
+	return arrivalsList
+
+
+while True:
+	arrivalsList = fetchBusArrivals()
+	for bus in arrivalsList:
+		lcd.clear()
+		tickerInfo = '%s to %s \nin %.0f minutes' % (bus[0], bus[1], bus[2])
+		lcd.message(tickerInfo)
+		time.sleep(3)
+		for i in range(16):
+			time.sleep(0.1)
+			lcd.move_left()
